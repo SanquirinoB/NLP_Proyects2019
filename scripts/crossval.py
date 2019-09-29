280f755d77d712c56792785d5f146c9be772faff
marcelo <marcelo.navarro@ing.uchile.cl>
2019-09-29 19:50:25 -0300

    added statistics data and plots

--- scripts/crossval.py
+++ scripts/crossval.py
@@ -1,3 +1,6 @@
+from sklearn.model_selection import cross_val_score
+import numpy as np
+
 def statisticsCV(clf, dataset, n_iter=10, eval_metric='accuracy'):
     metrics = cross_val_score(clf, dataset.tweet, dataset.sentiment_intensity, cv= n_iter, scoring=eval_metric)
     mean = np.mean(metrics)
