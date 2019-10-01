e5926ac807168081517edb2a3cf119194a03bc79
marcelo <marcelo.navarro@ing.uchile.cl>
2019-09-30 22:19:37 -0300

    cross validation in all clfs

--- scripts/manual_tokenizer.py
+++ scripts/manual_tokenizer.py
@@ -1,18 +1,26 @@
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import TweetTokenizer
 from nltk.sentiment.util import mark_negation
+from nltk.corpus import stopwords
+import string
+import nltk
 
 
 def overkill_tokenize(sentence):
+    punt = list(string.punctuation)
+    punt.remove("!")
+    remove_words = stopwords.words('english') + punt
     sentence = sentence.lower()
     # first: tweet tokenize
-    tweetTok = TweetTokenizer(strip_handles=True, reduce_len=False).tokenize
+    tweetTok = TweetTokenizer(strip_handles=True, reduce_len=True).tokenize
     lemma = WordNetLemmatizer().lemmatize
     preprocess_tweet = tweetTok(sentence)
+    
     # second: lemmatization
     preprocess_tweetlemma = []
     for word in preprocess_tweet:
-        preprocess_tweetlemma.append(lemma(word))
+        if not word in remove_words:
+            preprocess_tweetlemma.append(lemma(word))
     # third: add mark_negation
     preprocess_tweetLemmaMark = mark_negation(preprocess_tweetlemma)
     return preprocess_tweetLemmaMark
